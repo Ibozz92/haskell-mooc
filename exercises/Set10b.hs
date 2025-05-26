79d85485ac19167d139391ced3bbae8038462b79
Ibozz92 <bozzutoi@darienps.org>
2025-05-26 19:39:53 -0400

    Set10b is interesting

--- exercises/Set10b.hs
+++ exercises/Set10b.hs
@@ -22,7 +22,8 @@ import Mooc.Todo
 --   False ||| undefined ==> an error!
 
 (|||) :: Bool -> Bool -> Bool
-x ||| y = todo
+_ ||| True = True
+a ||| False = a
 
 ------------------------------------------------------------------------------
 -- Ex 2: Define the function boolLength, that returns the length of a
@@ -36,7 +37,9 @@ x ||| y = todo
 --   length [False,undefined] ==> 2
 
 boolLength :: [Bool] -> Int
-boolLength xs = todo
+boolLength [] = 0
+boolLength (x:xs) = if x then a else a
+                    where a = 1 + boolLength xs
 
 ------------------------------------------------------------------------------
 -- Ex 3: Define the function validate which, given a predicate and a
@@ -50,7 +53,7 @@ boolLength xs = todo
 --   validate (\x -> undefined) 3  ==>  an error!
 
 validate :: (a -> Bool) -> a -> a
-validate predicate value = todo
+validate predicate value = if (predicate value) then value else value
 
 ------------------------------------------------------------------------------
 -- Ex 4: Even though we can't implement the generic seq function
@@ -84,10 +87,12 @@ class MySeq a where
   myseq :: a -> b -> b
 
 instance MySeq Bool where
-  myseq = todo
+  myseq thebool stupidthing = if thebool then stupidthing else stupidthing
 
 instance MySeq Int where
-  myseq = todo
+  myseq 0 stupidthing = stupidthing
+  myseq theint stupidthing = myseq (theint-theint) stupidthing
 
 instance MySeq [a] where
-  myseq = todo
+  myseq [] stupidthing = stupidthing
+  myseq (x:xs) stupidthing = stupidthing
